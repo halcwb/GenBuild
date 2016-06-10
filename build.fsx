52f504854c730c763db7a6ab2a834fce334cecf5
Casper Bollen <halcwb@gmail.com>
2016-06-10 12:39:45 +0200

    Removed running targets from targets.fsx

--- build.fsx
+++ build.fsx
@@ -5,3 +5,49 @@ System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 #load "paket-files/halcwb/GenBuild/scripts/targets.fsx"
 
+// --------------------------------------------------------------------------------------
+// Run all targets by default. Invoke 'build <Target>' to override
+
+
+Target "All" DoNothing
+
+
+"Clean"
+  ==> "AssemblyInfo"
+  ==> "Build"
+  ==> "CopyBinaries"
+  ==> "RunTests"
+  ==> "GenerateReferenceDocs"
+  ==> "GenerateDocs"
+  ==> "All"
+  =?> ("ReleaseDocs",isLocalBuild)
+
+"All" 
+#if MONO
+#else
+  =?> ("SourceLink", Pdbstr.tryFind().IsSome )
+#endif
+  ==> "NuGet"
+  ==> "BuildPackage"
+
+"CleanDocs"
+  ==> "GenerateHelp"
+  ==> "GenerateReferenceDocs"
+  ==> "GenerateDocs"
+
+"CleanDocs"
+  ==> "GenerateHelpDebug"
+
+"GenerateHelp"
+  ==> "KeepRunning"
+    
+"ReleaseDocs"
+  ==> "Release"
+
+"BuildPackage"
+  ==> "PublishNuget"
+  ==> "Release"
+
+
+RunTargetOrDefault "All"
+
