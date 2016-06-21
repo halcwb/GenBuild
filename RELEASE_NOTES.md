3c49e510c1416f77080ee8b0d9c0eb9e3f8bc5cb
Casper Bollen <halcwb@gmail.com>
2016-06-22 00:08:12 +0200

    Bump version to 0.2.0-beta

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,7 +1,10 @@
+#### 0.2.0-beta - June 22 2016
+* Updated nunit to use latest version
+* FsCheck now without nunit integration
+
 #### 0.1.0-beta - June 07 2016
 * **Note: This is a dummy library to test the use of a general build script!**
 * updated targets, build.fsx now contains running the targets
-
 
 #### 0.0.1-alpha - May 22 2016
 * **Note: This is a dummy library to test the use of a general build script!**
--- src/GenBuild/AssemblyInfo.fs
+++ src/GenBuild/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("GenBuild")>]
 [<assembly: AssemblyCompanyAttribute("halcwb")>]
 [<assembly: AssemblyDescriptionAttribute("A dummy library to test a generic build script")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
-    let [<Literal>] InformationalVersion = "0.1.0"
+    let [<Literal>] Version = "0.2.0"
+    let [<Literal>] InformationalVersion = "0.2.0"
